88986d1bf9565384bed376bbf9a9780546840152
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:52:43 -0300

    Add files via upload

---
+++ 85-PromedioDeValoresDeArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//Hacer la media de los valores de un array y mostrar el resultado.
+
+namespace _85_PromedioDeValoresDeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = { 1, 2, 3, 4, 5, 6 };
+
+            int suma = 0;
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                suma += valores[i];
+            }
+
+            double media = (double)suma / valores.Length;
+
+            Console.WriteLine("La media es " + media);
+        }
+    }
+}
